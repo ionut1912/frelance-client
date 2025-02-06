f2557afd09b8162fb80cb4388b6092b04c397c3c
Cornel <cornel.nitescu@plainconcepts.com>
2025-02-06 10:05:37 +0200

    fix code

--- terraform/appservice.tf
+++ terraform/appservice.tf
@@ -1,24 +1,24 @@
 resource "azurerm_service_plan" "freelance_client_plan" {
-  name                = "frelance-client-service-plan"
+  name                = "freelance-client-service-plan"
   location            = azurerm_resource_group.freelance_client_rg.location
   resource_group_name = azurerm_resource_group.freelance_client_rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
+
 resource "azurerm_linux_web_app" "freelance_client" {
   name                = "freelance-client"
   location            = azurerm_resource_group.freelance_client_rg.location
   resource_group_name = azurerm_resource_group.freelance_client_rg.name
   service_plan_id     = azurerm_service_plan.freelance_client_plan.id
-  https_only     = true
+  https_only          = true
 
   site_config {
     always_on = false
-    application_stack {
-      docker_image     = "${azurerm_container_registry.acr.login_server}/frelance-api"
-      docker_image_tag = "latest"
-    }
 
+    application_stack {
+      docker_image_name = "${azurerm_container_registry.acr.login_server}/freelance-client:latest"
+    }
   }
 
   identity {
