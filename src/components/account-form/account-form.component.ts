4cdbde6bf406fe97c53dea38a2c0f40d2a57f1c5
Cornel <cornel.nitescu@plainconcepts.com>
2025-02-18 11:12:15 +0200

    test2

--- src/components/account-form/account-form.component.ts
+++ src/components/account-form/account-form.component.ts
@@ -72,8 +72,7 @@ export class AccountFormComponent implements OnInit {
   }
 
   onSubmit(): void {
-    if (this.form.invalid) {
-      this.form.markAllAsTouched();
+    if (this.form.invalid) {this.form.markAllAsTouched();
       return;
     } else if (this.form.valid) {
       if (this.mode === 'login') {
