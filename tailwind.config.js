ffabc72410314d71b29fc3fac8cda3ee8b85de64
Cornel <cornel.nitescu@plainconcepts.com>
2025-02-18 11:11:07 +0200

    add husky and fix audit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}", // Ensure this points to your HTML and TS files
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
